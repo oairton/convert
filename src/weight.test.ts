4ddf9a3570be6977a397afb220fa0db58ac21b5d
airtong <airton-carvalho@outlook.com>
2024-05-04 22:53:03 -0300

    [test] revert "removing test of weight"

--- src/weight.test.ts
+++ src/weight.test.ts
@@ -28,6 +28,14 @@ describe('WeightAdapter', () => {
 
       expect(weightAdapter.see()).toBe('10 g');
     });
+
+    test('should throw an error for an invalid unit', () => {
+      const weightAdapter = new WeightAdapter();
+
+      expect(() => {
+        weightAdapter.create(10, 'invalid_unit');
+      }).toThrowError("Unidade de medida 'invalid_unit' não suportada");
+    });
   });
 
   describe('Testing mock of getOptions', () => {
